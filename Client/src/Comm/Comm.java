9e6603c73ac1f32c9191b21cb176bb0e98f541e4
Gal <gzsh@ymail.com>
2017-03-25 19:30:18 +0300

    JSON to Map Missing: javax.json, iteration over JSON

--- Client/src/Comm/Comm.java
+++ Client/src/Comm/Comm.java
@@ -6,8 +6,22 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Map;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
 
 public class Comm {
+    /**
+     * Gets a URL, and returns the page's content. Works only on plain text.
+     * In case of failure, returns null.
+     * @param Url
+     *  Page's URL, as a string.
+     * @return the page's content.
+     */
     public static String GetURLContent(String Url) {
         try {
             URL url = new URL(Url);
@@ -19,11 +33,22 @@ public class Comm {
             return br.readLine();
         } catch (MalformedURLException e) {
             return null;
-            // e.printStackTrace();
         } catch (IOException e) {
             return null;
-            // e.printStackTrace();
         }
-        // return null;
+    }
+
+    /**
+     * Converts a JSON-formatted string into a map.
+     * @param JSONrawData
+     *  JSON-formatted string
+     * @return the data given in JSON format as a map.
+     */
+    public static Map JSONtoMap(String JSONrawData) {
+        JsonReader jsonReader = Json.createReader(JSONrawData);
+        JsonObject jsonObject = jsonReader.readObject();
+        jsonReader.close();
+
+        // iterate JSON fields
     }
 }
