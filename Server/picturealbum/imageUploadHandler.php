46f74c8618f1021bf5069db55f88e130563286b0
Gal <gzsh@ymail.com>
2017-03-31 09:30:55 +0300

    Additional error (for unknown reason), more general code ($_FILE["field"] is delivered by c'tor), change self:: (static) to $this where needed.

--- Server/picturealbum/imageUploadHandler.php
+++ Server/picturealbum/imageUploadHandler.php
@@ -1,36 +1,41 @@
 <?php
     class uploadHandler {
         private static $target_dir = "uploads/";
+        private $file;
+
+        public function __construct($file) {
+            $this->file = $file;
+        }
 
         public static function setTargetDir($path) {
             self::$target_dir = $path;
         }
 
         private function getFileName() {
-            return $_FILES["fileToUpload"]["name"];
+            return ($this->file)["name"];
         }
 
         private function getFileTempName() {
-            return $_FILES["fileToUpload"]["tmp_name"];
+            return ($this->file)["tmp_name"];
         }
 
         private function getFileType() {
-            return $_FILES["fileToUpload"]["type"];
+            return ($this->file)["type"];
         }
 
         private function getFileSize() {
-            return $_FILES["fileToUpload"]["size"];
+            return ($this->file)["size"];
         }
 
         private function IsAnImage() {
-            $file_type = self::getFileType();
+            $file_type = $this->getFileType();
             if ($file_type == "image/jpg" || $file_type == "image/jpeg" || $file_type == "image/gif" || $file_type == "image/png")
                 return true;
             return false;
         }
 
         private function isFileNameExists() {
-            if (file_exists(self::$target_dir . basename(self::getFileName()))) {
+            if (file_exists(self::$target_dir . basename($this->getFileName()))) {
                 return true;
             }
             return false;
@@ -44,7 +49,7 @@
         }
 
         private function moveDirectory() {
-            move_uploaded_file(self::getFileTempName(), self::$target_dir . basename(self::getFileName()));
+            return move_uploaded_file($this->getFileTempName(), self::$target_dir . basename($this->getFileName()));
         }
 
         public function uploadFile() {
@@ -60,10 +65,12 @@
                 return json_encode(array("Error" => "File exceeded size limit."));
             }
 
-            $this->moveDirectory();
-            return json_encode(array("Error" => "File successfully uploaded."));
+            if ($this->moveDirectory()) {
+                return json_encode(array("Error" => "File successfully uploaded."));
+            }
+            return json_encode(array("Error" => "An error occurred while uploading the file."));
         }
     }
 
-    echo (new uploadHandler())->uploadFile();
+    echo (new uploadHandler($_FILES["fileToUpload"]))->uploadFile();
 ?>
