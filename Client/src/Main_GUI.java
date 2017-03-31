ad4ece3394caea220e0b41a95aed0d357d4539d2
Gal <gzsh@ymail.com>
2017-03-31 15:25:42 +0300

    Main GUI form

--- Client/src/Main_GUI.java
+++ Client/src/Main_GUI.java
@@ -11,87 +11,82 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
-/**
- * Created by Mor on 30/03/2017.
- */
 public class Main_GUI extends JFrame {
+    private JButton[] btn = new JButton[7];
+    private String[] str = {"To Do List", "Memos", "Exchange Rates", "Picture Album", "On This Day", "RSS", "World Clocks"};
 
-
-    private JButton exchangeRatesButton;
-    private JButton worldClocksButton;
-    private JButton toDoListButton;
-    private JButton RSSButton;
-    private JButton pictureAlbumButton;
-    private JButton onThisDayButton;
-    private JButton memosButton;
-
-
-    public Main_GUI(){
+    public Main_GUI() {
         super("Select a App");
-        setLayout(new FlowLayout());
-        setVisible(true);
-        setSize(250,400);
+        this.setLayout(new FlowLayout());
+        this.setSize(250, 400);
 
+        for (int i = 0; i < btn.length; i++) {
+            btn[i] = new JButton(str[i]);
+            this.add(btn[i]);
+        }
 
-        add(exchangeRatesButton);
-        add(worldClocksButton);
-        add(toDoListButton);
-        add(RSSButton);
-        add(pictureAlbumButton);
-        add(onThisDayButton);
-        add(memosButton);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
 
-        onThisDayButton.addActionListener(new ActionListener() {
+        btn[0].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                OnThisDay_GUI otd_main=new OnThisDay_GUI();
-                otd_main.setVisible(true);
-            }
-        });
-        exchangeRatesButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ExchangeRates_GUI er =new ExchangeRates_GUI();
-                er.setVisible(true);
-            }
-        });
-        memosButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Mamo_GUI mm =new Mamo_GUI();
-                mm.setVisible(true);
-            }
-        });
-        pictureAlbumButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                PictureAlbum_GUI pa =new PictureAlbum_GUI();
-                pa.setVisible(true);
-            }
-        });
-        RSSButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                RSS_GUI rss =new RSS_GUI();
-                rss.setVisible(true);
-            }
-        });
-        toDoListButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ToDoList_GUI tdl =new ToDoList_GUI();
+                ToDoList_GUI tdl = new ToDoList_GUI();
                 tdl.setVisible(true);
             }
         });
-        worldClocksButton.addActionListener(new ActionListener() {
+
+        btn[1].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                WorldClocks_GUI wc =new WorldClocks_GUI();
+                Mamo_GUI mm = new Mamo_GUI();
+                mm.setVisible(true);
+            }
+        });
+
+        btn[2].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ExchangeRates_GUI er = new ExchangeRates_GUI();
+                er.setVisible(true);
+            }
+        });
+
+        btn[3].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PictureAlbum_GUI pa = new PictureAlbum_GUI();
+                pa.setVisible(true);
+            }
+        });
+
+        btn[4].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OnThisDay_GUI otd_main = new OnThisDay_GUI();
+                otd_main.setVisible(true);
+            }
+        });
+
+        btn[5].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                RSS_GUI rss = new RSS_GUI();
+                rss.setVisible(true);
+            }
+        });
+
+        btn[6].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                WorldClocks_GUI wc = new WorldClocks_GUI();
                 wc.setVisible(true);
             }
         });
     }
 
-
+    public static void main(String[] args) {
+        new Main_GUI();
+    }
 }
