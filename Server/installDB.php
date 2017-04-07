005f7ef9e1fbde6f1dbbc4254481c18fa600e677
Gal <gzsh@ymail.com>
2017-04-07 14:30:59 +0300

    Slim

--- Server/installDB.php
+++ Server/installDB.php
@@ -1,4 +1,23 @@
 <?php
+    $config['displayErrorDetails'] = true;
+    $config['addContentLengthHeader'] = false;
+
+    $config['db']['host']   = "localhost";
+    $config['db']['user']   = "root";
+    $config['db']['pass']   = "";
+    $config['db']['dbname'] = "Widgets";
+
+    $app = new \Slim\App(["settings" => $config]);
+
+    $container['db'] = function ($c) {
+        $db = $c['settings']['db'];
+        $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+            $db['user'], $db['pass']);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
+    };
+
     include "SQLManipulator.php";
     $SQL = SQLManipulator::getInstance('localhost', 'root', '', 'Widgets');
 
