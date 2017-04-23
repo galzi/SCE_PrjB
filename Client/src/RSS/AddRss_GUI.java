105bfe9a4fbd875637f8888a1c26195eeafa4ede
Or <orgalil21@gmail.com>
2017-04-23 23:11:13 +0300

    AddRss_Gui as singleton. add label.

--- Client/src/RSS/AddRss_GUI.java
+++ Client/src/RSS/AddRss_GUI.java
@@ -23,36 +23,54 @@ import javax.swing.JButton;
  */
 public class AddRss_GUI extends JFrame {
 
-        private JPanel contentPane;
+    private static AddRss_GUI instance = null;
 
-        public AddRss_GUI() {
-                super("AddRss");
-                setLayout(new BorderLayout());
-                setVisible(true);
-                //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                setBounds(150, 70, 300, 150);
+    public AddRss_GUI() {
+        super("AddRss");
+        setLayout(new BorderLayout());
+        setVisible(true);
+        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(150, 70, 300, 150);
 
-                JPanel textPanel = new JPanel();
-                textPanel.setLayout(new FlowLayout());
+        JPanel textPanel = new JPanel();
+        textPanel.setLayout(new FlowLayout());
 
-                JTextField urlTextField = new JTextField();
-                urlTextField.setPreferredSize(new Dimension(300,30));
-                textPanel.add(urlTextField);
-                add(textPanel,BorderLayout.CENTER);
-
-
-                JPanel buttonsPanel = new JPanel();
-                buttonsPanel.setLayout(new FlowLayout());
-
-                JButton btnAddRss = new JButton("OK");
-                btnAddRss.setBounds(50,50,50,50);
-                buttonsPanel.add(btnAddRss);
-
-                JButton btnAbort = new JButton("Abort");
-                btnAbort.setBounds(50,50,50,50);
-                buttonsPanel.add(btnAbort);
-                add(buttonsPanel,BorderLayout.SOUTH);
+        JLabel urlLabel = new JLabel("Url feed");
+        urlLabel.setPreferredSize(new Dimension(50,20));
+        textPanel.add(urlLabel);
 
 
-        }
+        JTextField urlTextField = new JTextField("http://www.");
+        urlTextField.setPreferredSize(new Dimension(270,30));
+        textPanel.add(urlTextField);
+        add(textPanel,BorderLayout.CENTER);
+
+
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new FlowLayout());
+
+        JButton btnAddRss = new JButton("OK");
+        btnAddRss.setBounds(50,50,50,50);
+        btnAddRss.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        buttonsPanel.add(btnAddRss);
+
+        JButton btnAbort = new JButton("Abort");
+        btnAbort.setBounds(50,50,50,50);
+        buttonsPanel.add(btnAbort);
+        add(buttonsPanel,BorderLayout.SOUTH);
+    }
+
+    public static AddRss_GUI getInstance(){
+        if(instance == null)
+            instance = new AddRss_GUI();
+
+        return instance;
+    }
+
+
 }
