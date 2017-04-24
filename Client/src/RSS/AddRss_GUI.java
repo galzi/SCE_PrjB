513d3d978c53bbf18afce0ff49c4d5a6c1ff8c68
Or <orgalil21@gmail.com>
2017-04-24 22:56:57 +0300

    AddRss_GUI textBox get input addRss button responds partially

--- Client/src/RSS/AddRss_GUI.java
+++ Client/src/RSS/AddRss_GUI.java
@@ -1,5 +1,9 @@
 package RSS;
 
+import RSS.model.Feed;
+import RSS.model.FeedMessage;
+import RSS.read.RSSFeedParser;
+import com.sun.xml.internal.ws.api.model.ExceptionType;
 import javafx.scene.layout.Border;
 
 import javax.swing.*;
@@ -24,45 +28,77 @@ import javax.swing.JButton;
 public class AddRss_GUI extends JFrame {
 
     private static AddRss_GUI instance = null;
+    private JFrame frame;
 
     public AddRss_GUI() {
         super("AddRss");
         setLayout(new BorderLayout());
         setVisible(true);
         //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(150, 70, 300, 150);
+        setBounds(150, 70, 800, 600);
 
-        JPanel textPanel = new JPanel();
-        textPanel.setLayout(new FlowLayout());
-
-        JLabel urlLabel = new JLabel("Url feed");
-        urlLabel.setPreferredSize(new Dimension(50,20));
-        textPanel.add(urlLabel);
-
-
-        JTextField urlTextField = new JTextField("http://www.");
-        urlTextField.setPreferredSize(new Dimension(270,30));
-        textPanel.add(urlTextField);
-        add(textPanel,BorderLayout.CENTER);
+        JPanel NorthPanel = new JPanel();
+        JLabel rssListLabel = new JLabel("RSS list");
+        rssListLabel.setPreferredSize(new Dimension(50,20));
+        NorthPanel.add(rssListLabel);
+        add(NorthPanel,BorderLayout.NORTH);
 
 
-        JPanel buttonsPanel = new JPanel();
-        buttonsPanel.setLayout(new FlowLayout());
+        JPanel CenterPanel = new JPanel();
+        DefaultListModel listModel = new DefaultListModel();
+        listModel.addElement("Jane Doe");
+        listModel.addElement("John Smith");
+        listModel.addElement("Kathy Green");
+        JList rssList = new JList(listModel);
+        CenterPanel.add(rssList);
+        add(CenterPanel,BorderLayout.CENTER);
 
-        JButton btnAddRss = new JButton("OK");
+
+        listModel.addElement("Or Galili");
+
+        JPanel EastPanel = new JPanel();
+        JButton btnAbort = new JButton("Delete rss");
+        btnAbort.setBounds(50,50,50,50);
+        EastPanel.add(btnAbort);
+        add(EastPanel,BorderLayout.EAST);
+
+        JPanel SouthPanel = new JPanel();
+        SouthPanel.setLayout(new FlowLayout());
+        JButton btnAddRss = new JButton("Add rss");
         btnAddRss.setBounds(50,50,50,50);
+
+        SouthPanel.add(btnAddRss);
+        JTextField urlTextField = new JTextField("http://www.");
+        urlTextField.setPreferredSize(new Dimension(300,30));
+        SouthPanel.add(urlTextField);
+        add(SouthPanel,BorderLayout.SOUTH);
+
+
         btnAddRss.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String urlFeed = urlTextField.getText();
+                try {
+                    RSSFeedParser parser = new RSSFeedParser(urlFeed);/*http://www.vogella.com/article.rss*/
+                    Feed feed = parser.readFeed();
+                    listModel.addElement(feed.getDescription());
+                    //System.out.println(feed);
+                    for (FeedMessage message : feed.getMessages()) {
+                        System.out.println(message);
+                    }
+                }
+                catch (Exception el){
+                    //custom title, error icon
+                    JOptionPane.showMessageDialog(frame,
+                            "This is not rss",
+                            "Inane error",
+                            JOptionPane.ERROR_MESSAGE);
+                    urlTextField.setText("");
+                    return;
+                }
 
             }
         });
-        buttonsPanel.add(btnAddRss);
-
-        JButton btnAbort = new JButton("Abort");
-        btnAbort.setBounds(50,50,50,50);
-        buttonsPanel.add(btnAbort);
-        add(buttonsPanel,BorderLayout.SOUTH);
     }
 
     public static AddRss_GUI getInstance(){
