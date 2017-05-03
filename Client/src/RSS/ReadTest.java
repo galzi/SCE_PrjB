e73e445b2da856410ef1a60ced143aaa886d34cd
Or <orgalil21@gmail.com>
2017-05-03 08:58:11 +0300

    old rss gui deleted

--- Client/src/RSS/ReadTest.java
+++ Client/src/RSS/ReadTest.java
@@ -7,7 +7,7 @@ import RSS.read.RSSFeedParser;
 public class ReadTest {
     public static void main(String[] args) {
         RSSFeedParser parser = new RSSFeedParser(
-                "http://www.ksl.com/xml/148.rss");/*http://www.vogella.com/article.rss*/
+                "http://rss.cnn.com/rss/cnn_topstories.rss");/*http://www.vogella.com/article.rss*/
         Feed feed = parser.readFeed();
         System.out.println(feed);
         for (FeedMessage message : feed.getMessages()) {
