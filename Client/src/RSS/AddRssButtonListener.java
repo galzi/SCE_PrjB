54d92f204e814318b7347b96ccfd476e8ccfd820
Or <orgalil21@gmail.com>
2017-05-04 14:11:30 +0300

    AddRssButtonListener feedList added

--- Client/src/RSS/AddRssButtonListener.java
+++ Client/src/RSS/AddRssButtonListener.java
@@ -7,6 +7,8 @@ import RSS.read.RSSFeedParser;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Or on 5/3/2017.
@@ -16,25 +18,30 @@ public class AddRssButtonListener implements ActionListener {
     private JTextField urlTextField;
     private DefaultListModel rssListModel;
     private JFrame frame;
+    private ArrayList<Feed> feedList;
 
 
-    public AddRssButtonListener(JTextField urlTextField, DefaultListModel rssListModel){
+    public AddRssButtonListener(JTextField urlTextField, DefaultListModel rssListModel, ArrayList<Feed> feedList){
         //is this good build?
         this.urlTextField = urlTextField;
         this.rssListModel = rssListModel;
+        this.feedList = feedList;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         String urlFeed = urlTextField.getText();
         try {
+            //http://rssfeeds.usatoday.com/usatoday-NewsTopStories //error
             RSSFeedParser parser = new RSSFeedParser(urlFeed);
             Feed feed = parser.readFeed();
             rssListModel.addElement(feed.getDescription());
+            feedList.add(feed);
+            //feedList.remove(0);
             //System.out.println(feed);
-            for (FeedMessage message : feed.getMessages()) {
+            /*for (FeedMessage message : feed.getMessages()) {
                 System.out.println(message);
-            }
+            }*/
         }
         catch (Exception el){
             //custom title, error icon
@@ -42,7 +49,7 @@ public class AddRssButtonListener implements ActionListener {
                     "This is not rss",
                     "error",
                     JOptionPane.ERROR_MESSAGE);
-            urlTextField.setText("");
+            //urlTextField.setText("");
             return;
         }
 
