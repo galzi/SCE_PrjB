4daada69ff5636e723b3da1b10175539fa9d5a07
Or <orgalil21@gmail.com>
2017-05-04 14:13:32 +0300

    Delete/Open rss button listeners

--- Client/src/RSS/RssListMouseListener.java
+++ Client/src/RSS/RssListMouseListener.java
@@ -1,9 +1,15 @@
 package RSS;
 
+import RSS.model.Feed;
+import RSS.model.FeedMessage;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
 /**
  * Created by Or on 5/3/2017.
@@ -13,24 +19,67 @@ public class RssListMouseListener implements MouseListener {
     private JPopupMenu rssListPopMenu;
     private JMenuItem openRssItem;
     private JMenuItem deleteRssItem;
+    private ArrayList<Feed> feedList;
+    private DefaultListModel feedMessageListModel;
+    private JList rssList;
+    private ArrayList<String> feedLinks;
 
-    public RssListMouseListener(){
+    public RssListMouseListener(ArrayList<Feed> feedList, DefaultListModel feedMessageListModel,JList rssList,ArrayList<String> feedLinks){
+        this.feedList = feedList;
+        this.feedMessageListModel = feedMessageListModel;
+        this.rssList = rssList;
+        this.feedLinks = feedLinks;
         rssListPopMenu = new JPopupMenu();
         openRssItem = new JMenuItem("Open RSS");
+        buildOpenRssListener();
         deleteRssItem = new JMenuItem("Delete RSS");
+        buildDeleteRssListener();
         rssListPopMenu.add(openRssItem);
         rssListPopMenu.add(deleteRssItem);
+    }
+
+    public void buildOpenRssListener(){
+        openRssItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int row = rssList.getSelectedIndex();
+                showRssMessagesInJList(row);
+            }
+        });
+    }
+
+    public void buildDeleteRssListener(){
+        deleteRssItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int row = rssList.getSelectedIndex();
+                ((DefaultListModel)rssList.getModel()).remove(row);
+                feedList.remove(row);
+            }
+        });
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         JList rssList = (JList)e.getSource();
         if(isPressedOnItem(e)) {
+
             if (SwingUtilities.isRightMouseButton(e)) {
                 rssListPopMenu.show(e.getComponent(), e.getX(), e.getY());
             }
+
+            else if(SwingUtilities.isLeftMouseButton(e)){
+                if(e.getClickCount()==2){
+                    //feedMessageListModel.clear();
+                    int row = rssList.locationToIndex(e.getPoint());
+                    showRssMessagesInJList(row);
+                    //for (FeedMessage message : feedList.get(row).getMessages()) {
+                      //  feedMessageListModel.addElement(message);
+                        //System.out.println(message);
+                    }
+                }
+            }
         }
-    }
 
     @Override
     public void mousePressed(MouseEvent e) {
@@ -41,6 +90,7 @@ public class RssListMouseListener implements MouseListener {
                 rssList.setSelectedIndex(row);
             }
         }
+
     }
 
     @Override
@@ -48,12 +98,18 @@ public class RssListMouseListener implements MouseListener {
         isPressedOnItem(e);
     }
 
-    @Override
-    public void mouseEntered(MouseEvent e) {
-    }
+    private void showRssMessagesInJList(int rssIndex){
+        feedMessageListModel.clear();
+        feedLinks.clear();
+        for (FeedMessage message : feedList.get(rssIndex).getMessages()) {
+            feedLinks.add(message.getLink());
+            String msg = new String();
+            msg+="Title: " + message.getTitle() +"\n Description: "+ message.getDescription();
+            feedMessageListModel.addElement(msg);
 
-    @Override
-    public void mouseExited(MouseEvent e) {
+            //feedMessageListModel.addElement(message);
+            //System.out.println(message);
+        }
     }
 
     private boolean isPressedOnItem(MouseEvent e){
@@ -66,5 +122,13 @@ public class RssListMouseListener implements MouseListener {
         }
         rssList.clearSelection();
         return false;
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
     }
 }
