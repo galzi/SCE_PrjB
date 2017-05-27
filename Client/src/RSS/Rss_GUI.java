6507faf3fd79395c77615b9feef370448db3d49a
Or <orgalil21@gmail.com>
2017-05-27 23:19:22 +0300

    Rss MouseListener to MouseAdapter

--- Client/src/RSS/Rss_GUI.java
+++ Client/src/RSS/Rss_GUI.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.Border;
 import javax.swing.*;
 import java.awt.*;
 
+import java.awt.event.*;
 import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
@@ -17,10 +18,6 @@ import java.util.List;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -105,7 +102,7 @@ public class Rss_GUI extends JFrame {
 
         btnAddRss.addActionListener(new AddRssButtonListener(urlTextField,rssListModel,feedList));
         rssList.addMouseListener(new RssListMouseListener(feedList,feedMessageListModel,rssList,feedLinks));
-        rssMessages.addMouseListener(new MouseListener() {
+        rssMessages.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 if(SwingUtilities.isLeftMouseButton(e)){
@@ -117,35 +114,11 @@ public class Rss_GUI extends JFrame {
                             Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop():null;
                             desktop.browse(url.toURI());
                         }
-                        catch(Exception el){
-
-                        }
-
+                        catch(Exception el){}
                     }
                 }
             }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
         });
-
 
     }
 
