d073f56f32f03e216108ad705e75fdd376779a8e
or <orgalil21@gmail.com>
2017-06-04 11:11:01 +0300

    Login to system

---
+++ Server/login.php
@@ -0,0 +1,18 @@
+<?php
+    include "SQLManipulator.php";
+    $SQL = SQLManipulator::getInstance("localhost", "root", "", "Widgets");
+
+    if (!isset($_POST)) {
+        die();
+    } else {
+        $SQL->initializeConnection();
+        $query = $SQL->performQuery("SELECT * FROM users WHERE username = " . $_POST["username"] . " and password = " . $_POST["password"]);
+        if (sizeof($SQL->iterate($query)) == 1) {
+            session_start();
+            $_SESSION["username"] = $_POST["username"];
+            echo "{loginStatus: SUCCESS}";
+        } else { // == 0
+            echo "{loginStatus: FAILURE}";
+            die();
+        }
+    }
