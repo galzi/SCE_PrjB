a2fe313079c22f469b2aeeb1eaa168a1b9d33c05
Gal <gzsh@ymail.com>
2017-06-08 10:24:10 +0300

    Routes, Formatting

--- Server/login.php
+++ Server/login.php
@@ -1,21 +1,41 @@
 <?php
-    // include "SQLManipulator.php";
-    include "dbLogin.php";
+    class login {
+        private $SQL;
+        private $username;
+        private $pass;
 
-    if (!isset($_POST)) {
-        die();
-    } else {
-        $SQL->initializeConnection();
-        $query = $SQL->performQuery("SELECT * FROM users WHERE username = '" . $_POST["username"] . "' and password = '" . $_POST["password"] . "'");
+        public function __construct(mysqli $SQL, string $username, string $pass) {
+            $this->SQL = $SQL;
+            $this->username = $username;
+            $this->pass = $pass;
+        }
 
-        if (sizeof($SQL->iterate($query)) == 1) {
+        public function isCredentialsCorrect() { // isExists
+            return ($this->SQL->query("SELECT * FROM users WHERE username = '" . $this->username . "' and password = '" . $this->pass . "'"))->num_rows == 1;
+            // returns false if username doesn't exist or password incorrect
+        }
+
+        public function initSession() {
             session_start();
             $_SESSION["username"] = $_POST["username"];
-            // echo "{loginStatus: 'SUCCESS'}";
-            echo json_encode(array("loginStatus" => "SUCCESS"));
-        } else {
-            // echo "{loginStatus: 'FAILURE'}";
-            echo json_encode(array("loginStatus" => "FAILURE"));
-            die();
         }
+
+        public function returnResponse(bool $b) {
+            $str = $b ? "SUCCESS" : "FAILURE";
+            return json_encode(array("loginStatus" => $str));
+        }
+
+        public function login() {
+            if ($this->isCredentialsCorrect()) {
+                $this->initSession();
+                return $this->returnResponse(true);
+            }
+            return $this->returnResponse(false);
+        }
+    }
+
+    if (!isset($_POST["username"]) or !isset($_POST["password"])) {
+        die();
+    } else {
+        echo (new login(new mysqli("localhost", "root", "", "Widgets"), $_POST["username"], $_POST["password"]))->login();
     }
