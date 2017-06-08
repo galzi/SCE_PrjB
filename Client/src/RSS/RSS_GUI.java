187156b4f27ea602511602693a3624a0744fa051
Or <orgalil21@gmail.com>
2017-06-08 12:15:50 +0300

    Rss changes : all listeners in Rss_GUI file

--- Client/src/RSS/RSS_GUI.java
+++ Client/src/RSS/RSS_GUI.java
@@ -3,6 +3,7 @@ package RSS;
 import Comm.Comm;
 import Comm.HttpUrlConnection;
 import RSS.model.Feed;
+import RSS.model.FeedMessage;
 import RSS.read.RSSFeedParser;
 
 import javax.swing.*;
@@ -16,6 +17,8 @@ import java.util.Map;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Marshaller.Listener;
 
 import static Comm.Comm.toMap;
 
@@ -23,7 +26,7 @@ import static Comm.Comm.toMap;
 /**
  * Created by Or on 4/23/2017.
  */
-public class RSS_GUI extends JFrame {
+public class RSS_GUI extends JFrame implements ActionListener , MouseListener {
 
     private static RSS_GUI instance = null;
     private Container RssGuiPanel;
@@ -43,6 +46,10 @@ public class RSS_GUI extends JFrame {
     private ArrayList<Feed> feedList = new ArrayList<Feed>();
 
     private ArrayList<String> feedLinks = new ArrayList<String>();
+
+    private JPopupMenu rssListPopMenu;
+    private JMenuItem openRssItem;
+    private JMenuItem deleteRssItem;
 
 
     public RSS_GUI() {
@@ -91,25 +98,19 @@ public class RSS_GUI extends JFrame {
         scrollRssMessages = new JScrollPane(rssMessages);
         RssGuiPanel.add(scrollRssMessages,constraints);
 
-        btnAddRss.addActionListener(new AddRssButtonListener(urlTextField,rssListModel,feedList));
-        rssList.addMouseListener(new RssListMouseListener(feedList,feedMessageListModel,rssList,feedLinks));
-        rssMessages.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                if(SwingUtilities.isLeftMouseButton(e)){
-                    JList rssMessagesc = (JList)e.getSource();
-                    if(e.getClickCount()==2){
-                        int row = rssMessagesc.getSelectedIndex();
-                        try {
-                            URL url = new URL(feedLinks.get(row));
-                            Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop():null;
-                            desktop.browse(url.toURI());
-                        }
-                        catch(Exception el){}
-                    }
-                }
-            }
-        });
+        rssListPopMenu = new JPopupMenu();
+        openRssItem = new JMenuItem("Open RSS");
+        //buildOpenRssListener();
+        deleteRssItem = new JMenuItem("Delete RSS");
+        //buildDeleteRssListener();
+        rssListPopMenu.add(openRssItem);
+        rssListPopMenu.add(deleteRssItem);
+
+        openRssItem.addActionListener(this);
+        deleteRssItem.addActionListener(this);
+        btnAddRss.addActionListener(this);
+        rssList.addMouseListener(this);
+        rssMessages.addMouseListener(this);
 
         String URLs = null;
         try {
@@ -138,5 +139,132 @@ public class RSS_GUI extends JFrame {
             instance.setVisible(true);
 
         return instance;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == btnAddRss) {
+            String urlFeed = urlTextField.getText();
+            try {
+                System.out.println(HttpUrlConnection.GetPageContent(HttpUrlConnection.serverHost + "rss/?action=add&content=" + urlFeed));
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+
+            try {
+                //http://rssfeeds.usatoday.com/usatoday-NewsTopStories //error
+                RSSFeedParser parser = new RSSFeedParser(urlFeed);
+                Feed feed = parser.readFeed();
+                rssListModel.addElement(feed.getDescription());
+                feedList.add(feed);
+            } catch (Exception el) {
+
+                JOptionPane.showMessageDialog(this,
+                        "This is not rss",
+                        "error",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+        }
+        else if(e.getSource() == openRssItem){
+            int row = rssList.getSelectedIndex();
+            showRssMessagesInJList(row);
+        }
+        else if(e.getSource() == deleteRssItem){
+            int row = rssList.getSelectedIndex();
+            System.out.println(row);
+
+            try {
+                System.out.println(HttpUrlConnection.GetPageContent(HttpUrlConnection.serverHost+"rss/?action=del&content="+feedList.get(row).getLink()));
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+
+            ((DefaultListModel)rssList.getModel()).remove(row);
+            feedList.remove(row);
+        }
+
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if(e.getSource() == rssMessages){
+            if(SwingUtilities.isLeftMouseButton(e)){
+                JList rssMessagesc = (JList)e.getSource();
+                if(e.getClickCount()==2){
+                    int row = rssMessagesc.getSelectedIndex();
+                    try {
+                        URL url = new URL(feedLinks.get(row));
+                        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop():null;
+                        desktop.browse(url.toURI());
+                    }
+                    catch(Exception el){}
+                }
+            }
+        }
+        else if(e.getSource() == rssList) {
+            JList rssList = (JList) e.getSource();
+            if (isPressedOnItem(e)) {
+
+                if (SwingUtilities.isRightMouseButton(e)) {
+                    rssListPopMenu.show(e.getComponent(), e.getX(), e.getY());
+                } else if (SwingUtilities.isLeftMouseButton(e)) {
+                    if (e.getClickCount() == 2) {
+
+                        int row = rssList.locationToIndex(e.getPoint());
+                        showRssMessagesInJList(row);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        JList rssList = (JList)e.getSource();
+        if(isPressedOnItem(e)) {
+            if (SwingUtilities.isRightMouseButton(e)) {
+                int row = rssList.locationToIndex(e.getPoint());
+                rssList.setSelectedIndex(row);
+            }
+        }
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        isPressedOnItem(e);
+    }
+
+    private void showRssMessagesInJList(int rssIndex){
+        feedMessageListModel.clear();
+        feedLinks.clear();
+        for (FeedMessage message : feedList.get(rssIndex).getMessages()) {
+            feedLinks.add(message.getLink());
+            String msg = new String();
+            msg+="Title: " + message.getTitle() +"\n Description: "+ message.getDescription();
+            feedMessageListModel.addElement(msg);
+        }
+    }
+
+    private boolean isPressedOnItem(MouseEvent e){
+        JList rssList = (JList)e.getSource();
+        if(rssList.getModel().getSize()>0) {
+            Rectangle r = rssList.getCellBounds(rssList.getFirstVisibleIndex(), rssList.getLastVisibleIndex());
+            if (e.getPoint().getY() > r.getY() && e.getPoint().getY() < r.getMaxY()) {
+                return true;
+            }
+        }
+        rssList.clearSelection();
+        return false;
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
     }
 }
--- Client/src/RSS/RssFeed.java
+++
@@ -1,8 +0,0 @@
-package RSS;
-
-/**
- * Created by Or on 4/23/2017.
- */
-public class RssFeed {
-
-}
